7a29db652ee9b0c2c712af888145fa369675ee3b
ArthurDias01 <arthursantos01@gmail.com>
2021-07-05 10:36:17 -0300

    +sidebar filters +carousel

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,118 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React, { useState } from "react";
 
-import Product from './Product/Product';
-import useStyles from './styles';
+import Drawer from "@material-ui/core/Drawer";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
-const Products = ({ products, onAddToCart }) => {
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button, CardMedia } from "@material-ui/core";
+
+import Product from "./Product/Product";
+import useStyles from "./styles";
+
+const Products = ({ products, onAddToCart, categories }) => {
   const classes = useStyles();
 
+  const [categoryFiltered, setCategoryFiltered] = useState([]);
+
   if (!products.length) return <p>Loading...</p>;
+
+  const filterCategory = (category) => {
+    setCategoryFiltered(category);
+  };
+
+  let categoryName = {};
+  const productsWithCategories = products.map((item) => {
+    categoryName = { ...item.categories };
+    return { ...item, category: { ...categoryName[0] } };
+  });
+
+  const filtered = productsWithCategories.filter((item) => {
+    if (categoryFiltered != "") {
+      return item.category.name == `${categoryFiltered}`;
+    }
+    return true;
+  });
+
+  function Item(props) {
+    const { source } = props.item.media;
+    const { name } = props.item.name;
+    return (
+      <Paper className={classes.carouselpaper}>
+        <h2>{props.item.name}</h2>
+        <CardMedia
+          component="img"
+          className={classes.media}
+          image={source}
+          title={name}
+          height={100}
+          container
+        />
+        <Button className="CheckButton">Confira já!</Button>
+      </Paper>
+    );
+  }
 
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
-      <Grid container justify="center" spacing={4}>
-        {products.map((product) => (
+      <CssBaseline />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <Typography
+              className={classes.drawerTitle}
+              gutterBottom
+              variant="h4"
+            >
+              Categorias
+            </Typography>
+            {categories.map((category, index) => (
+              <ListItem
+                button
+                key={category.id}
+                onClick={() => filterCategory(category.name)}
+              >
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={category.name} />
+              </ListItem>
+            ))}
+            <ListItem button onClick={() => filterCategory([])}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              Limpar Filtros
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+
+      <Grid container justify="center" spacing={3}>
+        <Grid item xs={12} justify="center" spacing={3} alignItems="center">
+          <Carousel>
+            {filtered.map((item, i) => (
+              <Item key={i} item={item} />
+            ))}
+          </Carousel>
+        </Grid>
+        {filtered.map((product) => (
           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
             <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
@@ -24,4 +123,3 @@ const Products = ({ products, onAddToCart }) => {
 };
 
 export default Products;
-
--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,4 +1,6 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
 
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
@@ -6,8 +8,42 @@ export default makeStyles((theme) => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
+    paddingTop: theme.spacing(12),
+    display: "flex",
+    flexDirection: "row",
   },
   root: {
     flexGrow: 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: "auto",
+  },
+  drawerTitle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: theme.spacing(3),
+  },
+  media: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    objectFit: "contain",
+    height: 120,
+    width: "100%",
+  },
+  carouselpaper: {
+    display: "flex",
+    width: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
 }));
